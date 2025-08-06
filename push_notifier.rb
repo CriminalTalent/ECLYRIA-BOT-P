0d572b194372c40e49845176e62015367e9052e9
CriminalTalent <rntjdrud@naver.com>
2025-08-06 23:04:14 +0900

    Update push_notifier.rb

--- push_notifier.rb
+++ push_notifier.rb
@@ -1,1 +1,14 @@
+# push_notifier.rb
+
+module PushNotifier
+  def self.broadcast(mastodon_client, message)
+    return if message.nil? || message.strip.empty?
+
+    begin
+      mastodon_client.broadcast(message)
+    rescue => e
+      puts "[PushNotifier] 전송 실패: #{e.message}"
+    end
+  end
+end
 
