d2409e4a2223577ef40c1ee44ffd923e58a6cb1b
CriminalTalent <rntjdrud@naver.com>
2025-08-06 22:43:15 +0900

    Update enroll_command.rb

--- commands/enroll_command.rb
+++ commands/enroll_command.rb
@@ -1,1 +1,53 @@
+# commands/enroll_command.rb
+
+require 'date'
+
+class EnrollCommand
+  INITIAL_GALLEON = 20
+
+  def initialize(sheet, client, user_id, name)
+    @sheet = sheet
+    @client = client
+    @user_id = user_id
+    @name = name
+  end
+
+  def execute
+    user_sheet = @sheet.worksheet_by_title("사용자")
+    existing_row = find_user_row(user_sheet, @user_id)
+
+    if existing_row
+      @client.reply(@user_id, "#{@name}님은 이미 입학하셨습니다.")
+      return
+    end
+
+    new_row = [
+      @user_id,       # A: 마스토돈 ID
+      @name,          # B: 이름
+      INITIAL_GALLEON,# C: 갈레온
+      "",             # D: 아이템
+      "",             # E: 메모
+      "",             # F: 기숙사 (빈칸)
+      "",             # G: 마지막베팅일
+      "",             # H: 오늘베팅횟수
+      "",             # I: 출석날짜
+      "",             # J: 마지막타로일
+      0               # K: 개별 기숙사 점수
+    ]
+
+    user_sheet.insert_rows(user_sheet.num_rows + 1, [new_row])
+    user_sheet.save
+
+    @client.reply(@user_id, "#{@name}님, 입학을 확인했습니다. 열차에 탑승해 주세요.")
+  end
+
+  private
+
+  def find_user_row(sheet, id)
+    (2..sheet.num_rows).each do |row|
+      return row if sheet[row, 1] == id
+    end
+    nil
+  end
+end
 
