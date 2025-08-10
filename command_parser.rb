05c1d7af70f7081d1eab87eff9a72bdc2b392cbe
CriminalTalent <rntjdrud@naver.com>
2025-08-10 21:15:47 +0900

    Update command_parser.rb

--- command_parser.rb
+++ command_parser.rb
@@ -1,29 +1,42 @@
-# professor_command_parser.rb
-require_relative 'commands/enroll_command'
-require_relative 'commands/attendance_command'
-require_relative 'commands/homework_command'
+# command_parser.rb
+require_relative 'commands/battle_command'
+require_relative 'commands/potion_command'
+require_relative 'commands/investigate_command'
+require_relative 'commands/dm_investigation_command'
 
-module ProfessorParser
-  def self.parse(mastodon, sheet_manager, mention)
-    text = mention.status.content.gsub(/<[^>]*>/, '').strip
-    sender_full = mention.account.acct
+class CommandParser
+  def initialize(mastodon_client, sheet_manager)
+    @mastodon_client = mastodon_client
+    @sheet_manager = sheet_manager
+    
+    # 명령 처리 클래스 초기화
+    @battle = BattleCommand.new(@mastodon_client, @sheet_manager)
+    @potion = PotionCommand.new(@mastodon_client, @sheet_manager)
+    @investigate = InvestigateCommand.new(@mastodon_client, @sheet_manager)
+    @dm = DMInvestigationCommand.new(@mastodon_client, @sheet_manager)
+  end
+
+  def handle(status)
+    content = status.content.gsub(/<[^>]*>/, '').strip
+    sender_full = status.account.acct
     
     # sender ID 정규화 (@domain 부분 제거)
     sender = sender_full.split('@').first
     
-    puts "[교수봇] 처리 중: #{text} (from @#{sender_full} -> #{sender})"
+    puts "[전투봇] 처리 중: #{content} (from @#{sender_full} -> #{sender})"
     
     # 명령어 라우팅
-    case text
-    when /\[입학\/(.+?)\]/
-      name = $1.strip
-      EnrollCommand.new(sheet_manager, mastodon, sender, name).execute
-    when /\[출석\]/
-      AttendanceCommand.new(sheet_manager, mastodon, sender).execute
-    when /\[과제\]/
-      HomeworkCommand.new(sheet_manager, mastodon, sender).execute
+    case content
+    when /전투개시|DM전투개시|공격|방어|반격|도주/
+      @battle.handle(status)
+    when /물약사용/
+      @potion.handle(status)
+    when /조사|정밀조사|감지|훔쳐보기/
+      @investigate.handle(status)
+    when /DM조사결과/
+      @dm.handle(status)
     else
-      puts "[무시] 인식되지 않은 명령어: #{text}"
+      puts "[무시] 인식되지 않은 명령어: #{content}"
     end
   end
 end
