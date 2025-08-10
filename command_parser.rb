eb61cca4917d8814546286275a520cf75dfa77e3
CriminalTalent <rntjdrud@naver.com>
2025-08-10 17:22:24 +0900

    Update command_parser.rb

--- command_parser.rb
+++ command_parser.rb
@@ -6,9 +6,13 @@ require_relative 'commands/homework_command'
 module ProfessorParser
   def self.parse(mastodon, sheet_manager, mention)
     text = mention.status.content.gsub(/<[^>]*>/, '').strip
-    sender = mention.account.acct
-    puts "[교수봇] 처리 중: #{text}"
-
+    sender_full = mention.account.acct
+    
+    # sender ID 정규화 (@domain 부분 제거)
+    sender = sender_full.split('@').first
+    
+    puts "[교수봇] 처리 중: #{text} (from @#{sender_full} -> #{sender})"
+    
     # 명령어 라우팅
     case text
     when /\[입학\/(.+?)\]/
@@ -18,6 +22,8 @@ module ProfessorParser
       AttendanceCommand.new(sheet_manager, mastodon, sender).execute
     when /\[과제\]/
       HomeworkCommand.new(sheet_manager, mastodon, sender).execute
+    else
+      puts "[무시] 인식되지 않은 명령어: #{text}"
     end
   end
 end
