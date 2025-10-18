be54622f075a60af85aac21ccf385f7eb4f1afed
CriminalTalent <rntjdrud@naver.com>
2025-10-18 23:21:37 +0900

    Update homework_command.rb

--- commands/homework_command.rb
+++ commands/homework_command.rb
@@ -1,28 +1,27 @@
 # commands/homework_command.rb
 require 'date'
-require_relative '../house_score_updater'
+require_relative '../utils/house_score_updater'
 
 class HomeworkCommand
-  def initialize(sheet_manager, mastodon_client, sender)
+  def initialize(sheet_manager, mastodon_client, sender, status)
     @sheet_manager = sheet_manager
     @mastodon_client = mastodon_client
-    @sender = sender
+    @sender = sender.gsub('@', '')
+    @status = status
   end
 
   def execute
     # 1. 사용자 확인
     user = @sheet_manager.find_user(@sender)
     unless user
-      @mastodon_client.reply(@sender, "먼저 [입학/이름]으로 등록해주세요.")
-      return
+      return reply("먼저 [입학/이름]으로 등록해주세요.")
     end
 
     today = Date.today.to_s
-    
-    # 2. 과제 중복 확인 (last_bet_date 필드를 과제날짜로 임시 사용)
+
+    # 2. 과제 중복 확인
     if user[:last_bet_date] == today
-      @mastodon_client.reply(@sender, "오늘은 이미 과제를 제출하셨습니다.")
-      return
+      return reply("오늘은 이미 과제를 제출하셨습니다.")
     end
 
     # 3. 과제 제출 처리
@@ -33,6 +32,12 @@ class HomeworkCommand
     # 4. 기숙사 점수 갱신
     update_house_scores(@sheet_manager)
 
-    @mastodon_client.reply(@sender, "과제 제출 확인 하였습니다. 5갈레온과 기숙사 점수 3점을 지급하겠습니다. 수고하셨어요.")
+    reply("과제 제출 확인 하였습니다. 5갈레온과 기숙사 점수 3점을 지급하겠습니다. 수고하셨어요.")
+  end
+
+  private
+
+  def reply(message)
+    @mastodon_client.reply(@status, message)
   end
 end
